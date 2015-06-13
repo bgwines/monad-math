1045fa5e55e0554efe6a56b8dfa652b5a2260c58
Brett Wines <brettwines@gmail.com>
2015-06-13 02:46:04 -0700

    natural transformation tests

--- monad.hs
+++ monad.hs
@@ -1,19 +1,22 @@
 {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 import Control.Applicative
 import Control.Monad
 import Test.QuickCheck
 import Test.QuickCheck.Arbitrary
 
-data HaskObj
-    = TypeA
-    | TypeB deriving (Show, Eq, Enum, Bounded)
+type HaskObj = Int
 
-instance Arbitrary HaskObj where
-    arbitrary :: Gen HaskObj
-    arbitrary = do
-        x <- choose (False, True)
-        return (if x == True then TypeA else TypeB)
+--instance Arbitrary HaskObj where
+--    arbitrary :: Gen HaskObj
+--    arbitrary = do
+--        x <- choose (False, True)
+--        return (if x == True then TypeA else TypeB)
+
+--instance (Applicative f) => Eq (f HaskObj) where
+--    (==) :: f HaskObj -> f HaskObj -> Bool
+--    fa == fb = liftA2 (==) fa fb == pure True
 
 -- combinators
 
@@ -23,6 +26,7 @@ instance Arbitrary HaskObj where
 -- value constructors are one-to-one with types.
 (=->) :: a -> b -> (a -> b)
 a =-> b = \a' -> b
+--a =-> b = \a' -> if a == a' then b else undefined
 
 -- vertical composition
 (•) :: (a -> (ga -> ha)) -> (a -> (fa -> ga)) -> (a -> (fa -> ha))
@@ -38,11 +42,38 @@ fObj = pure
 fArr :: (Functor f, Applicative f) => (a -> b) -> (f a -> f b)
 fArr = fmap
 
+f2Obj :: (Functor f, Applicative f) => a -> f (f a)
+f2Obj = fObj . fObj
+
+f2Arr :: (Functor f, Applicative f) => (a -> b) -> (f (f a) -> f (f b))
+f2Arr = fArr . fArr
+
 -- the natural transformations
 
 -- µ : F^2 -.> F
 µ :: (Functor f, Applicative f) => a -> (f (f a) -> f a)
 µ a = (fObj . fObj $ a) =-> (fObj a)
+
+{-
+          h
+      x -----> y
+
+
+         f2 h
+    f2 x ----> f2 y
+    |          |
+    | µ x      | µ y
+    |          |
+    v          v
+    f x -----> f y
+         f h
+
+-}
+testµ :: (HaskObj, HaskObj) -> Bool
+testµ (x, y) = (µ y . f2Arr h $ [[x]]) == (fArr h . µ x $ [[x]])
+    where
+        h :: HaskObj -> HaskObj
+        h = x =-> y
 
 µf :: (Functor f, Applicative f) => a -> (f (f (f a)) -> f (f a))
 µf = µ . fObj
@@ -62,6 +93,30 @@ testLaw1 a = µµf a [[[a]]] == µfµ a [[[a]]]
 -- η : I -.> F
 η :: (Functor f, Applicative f) => a -> (a -> f a)
 η a = a =-> fObj a
+
+{-
+          h
+      x -----> y
+
+
+         i h
+    i x ----> i y
+    |          |
+    | η x      | η y
+    |          |
+    v          v
+    f x -----> f y
+         f h
+
+-}
+testη :: (HaskObj, HaskObj) -> Bool
+testη (x, y) = (η y . id h $ id x) == (fArr' h . η x $ id x)
+    where
+        h :: HaskObj -> HaskObj
+        h = x =-> y
+
+        fArr' :: (a -> b) -> ([a] -> [b])
+        fArr' = map
 
 fη :: (Functor f, Applicative f) => a -> f a -> f (f a)
 fη = fArr . η
@@ -83,5 +138,7 @@ testLaw2 a = and
 
 main :: IO ()
 main = do
+    quickCheckWith stdArgs { maxSuccess = 50 } testµ
+    quickCheckWith stdArgs { maxSuccess = 50 } testη
     quickCheckWith stdArgs { maxSuccess = 50 } testLaw1
     quickCheckWith stdArgs { maxSuccess = 50 } testLaw2
